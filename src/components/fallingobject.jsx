33285d02e524127b46c2707fe8c906f71b405e3a
aniOfri <46932810+aniOfri@users.noreply.github.com>
2022-05-02 21:33:56 +0200

    falling flags

--- src/components/fallingobject.jsx
+++ src/components/fallingobject.jsx
@@ -1,30 +1,49 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {RandInt} from '../modules/Calculators';
+import {countries} from '../data/ISO3166-1.js';
 
 
 function Object(props){
-    const [posX, setPosX] = useState(RandInt(props.width));
-    const [posY, setPosY] = useState(0);
-    const [object, setObject] = useState("Hello")
+    var line = countries[RandInt(countries.length)];
+    let url = "https://flagcdn.com/16x12/"+line.toLowerCase()+".png"
+
+    const [posX, setPosX] = useState(RandInt(props.width*0.9));
+    const [posY, setPosY] = useState(-50);
+    const [object, setObject] = useState(<img src={url} width="20"/>)
     const [lifeSpan, setLifeSpan] = useState(props.lifeSpan)
 
     useEffect(() => {
         let interval = null;
     
-        interval = setInterval(() => {
-            setPosY(posY + 1);
-            setLifeSpan(lifeSpan - 0.1);
-        }, 10);
+        if (lifeSpan > 0){
+            interval = setInterval(() => {
+                setPosY(posY + props.speed);
+                setLifeSpan(lifeSpan - props.speed);
+            }, 10);
+        }
+        else{
+            setPosX(RandInt(props.width*0.9))
+            setPosY(-50);
+            setLifeSpan(props.lifeSpan)
+            clearInterval(interval);
+            
+            line = countries[RandInt(countries.length)];
+            url = "https://flagcdn.com/16x12/"+line.toLowerCase()+".png"
+            setObject(<img src={url} width="20"/>)
+        }
+
         return () => {
             clearInterval(interval);
         };
-      }, [lifeSpan]);
+    }, [lifeSpan]);
     
+    let opacity = lifeSpan/props.lifeSpan*100+"%"
     const styles = {
         color: "white",
         position: "absolute",
         top: posY,
-        left: posX
+        left: posX,
+        opacity: opacity
       };
 
     return (
@@ -35,7 +54,11 @@ function Object(props){
 }
 
 function FallingObject(props){
-    const [objects, setObjects] = useState([<Object width={props.width} lifeSpan={200}/>])
+    let objs = [];
+    for (let i = 0; i < 50; i++){
+        objs.push(<Object index={i} width={props.width} lifeSpan={RandInt(800)} speed={RandInt(7)+3}/>)
+    }
+    const [objects, setObjects] = useState(objs)
 
 
     return (
